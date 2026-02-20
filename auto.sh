867c25c80e27355bf2933e0d5e4c3cdcb07165e3
Potted <ilorn.mc@gmail.com>
2026-02-20 13:24:01 +0800

    Clean auto-generated files after build
    - Update auto.sh to clean autoreconf output after build
    - Add configure, .dirstamp to .gitignore
    - Keep configure as exception (needed for builds without autoreconf)

--- auto.sh
+++ auto.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+rm -rf build/* autom4te.cache
+rm -f aclocal.m4 autom4te.cache configure compile depcomp install-sh missing Makefile.in ltmain.sh test-driver configure~
+
 autoreconf -fi
 
 mkdir -p build
@@ -13,3 +16,5 @@ make
 cd ..
 
 ln -sf build/spacefn ./spacefn
+
+rm -rf autom4te.cache aclocal.m4 configure~ compile depcomp install-sh missing Makefile.in ltmain.sh test-driver
